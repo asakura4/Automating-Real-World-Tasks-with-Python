fea28661a25590ee7678bc2891f04d0c16241d5b
Tzu-Hao Wang <asakura4@gmail.com>
2020-05-17 10:32:05 -0700

    Add Problem Statement

--- Module1.py
+++ Module1.py
@@ -1,3 +1,21 @@
+'''
+Qwiklabs Assessment: Scale and convert images using PIL
+Introduction
+Your company is in the process of updating its website, and they’ve hired a design contractor to 
+create some new icon graphics for the site. But the contractor has delivered the final designs in the wrong format 
+-- rotated 90° and too large. Oof! You’re not able to get in contact with the designers and your own deadline is approaching fast.
+You’ll need to use Python to get these images ready for launch.
+
+What you’ll do
+Use the Python Imaging Library to do the following to a batch of images:
+
+Open an image
+Rotate an image
+Resize an image
+Save an image in a specific format in a separate directory
+'''
+
+
 #!/usr/bin/env python3
 
 import os
@@ -19,6 +37,5 @@ for infile in os.listdir(inFolderPath):
 		with Image.open(inFolderPath + infile) as im:
 			new_im = im.convert('RGB').transpose(method=Image.ROTATE_90).resize(SIZE)
 			new_im.save(outFile, "JPEG")
-			print(outFile)
 	except:
 		pass
